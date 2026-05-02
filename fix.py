43b876b064df0668f6027401e723521d9ceaf9e4
lebed <lebedevegor.ekb@gmail.com>
2026-05-03 03:01:30 +0500

    fix: placeholder ScenarioEdit to unblock build

---
+++ fix.py
@@ -0,0 +1,3 @@
+﻿with open('src/pages/admin/ScenarioEdit.tsx', 'w', encoding='utf-8', newline='\n') as f:
+    f.write('export default function AdminScenarioEdit() { return <div>admin edit</div>; }\n')
+print('done')
