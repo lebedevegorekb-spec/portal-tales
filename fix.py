d82cebcd86b5dc10b9ecc8744d2fffe10304d4cb
lebed <lebedevegor.ekb@gmail.com>
2026-05-07 00:10:40 +0500

    chore: remove temp python scripts

--- fix.py
+++
@@ -1,3 +0,0 @@
-﻿with open('src/pages/admin/ScenarioEdit.tsx', 'w', encoding='utf-8', newline='\n') as f:
-    f.write('export default function AdminScenarioEdit() { return <div>admin edit</div>; }\n')
-print('done')
