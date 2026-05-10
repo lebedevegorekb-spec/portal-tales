8881a14c484354faed0c94a0415cbaff549306ff
lebed <lebedevegor.ekb@gmail.com>
2026-05-11 00:15:42 +0500

    debug: enable sourcemaps

---
+++ patch_sourcemap.py
@@ -0,0 +1,12 @@
+﻿with open('vite.config.ts', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Добавить sourcemap в build
+content = content.replace(
+    'export default defineConfig(({ mode }) => ({',
+    'export default defineConfig(({ mode }) => ({\n  build: { sourcemap: true },'
+)
+
+with open('vite.config.ts', 'w', encoding='utf-8', newline='\n') as f:
+    f.write(content)
+print('done')
