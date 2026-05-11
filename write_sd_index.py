9b4549844525b69fe91a4132a2515bd299df7254
lebed <lebedevegor.ekb@gmail.com>
2026-05-11 23:16:27 +0500

    chore: remove temp scripts

--- write_sd_index.py
+++
@@ -1,4 +0,0 @@
-﻿f = open('src/mechanics/SituationDeduction/index.ts', 'w', encoding='utf-8', newline='\n')
-f.write('export { SituationDeductionHost } from "./HostView";\nexport { SituationDeductionPlayer } from "./PlayerView";\n')
-f.close()
-print('done')
