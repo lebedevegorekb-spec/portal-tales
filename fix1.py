539bce45493b30a23c4830ff0aa815e9d3b1d19e
lebed <lebedevegor.ekb@gmail.com>
2026-05-15 16:24:46 +0500

    fix: pass players to mechanics

---
+++ fix1.py
@@ -0,0 +1,8 @@
+﻿f = open('src/mechanics/types.ts', encoding='utf-8')
+c = f.read()
+f.close()
+old = '  onAdvance?: () => Promise<void>;\n}'
+new = '  onAdvance?: () => Promise<void>;\n  players?: { id: string; display_name: string }[];\n}'
+c = c.replace(old, new)
+open('src/mechanics/types.ts', 'w', encoding='utf-8', newline='\n').write(c)
+print('done')
