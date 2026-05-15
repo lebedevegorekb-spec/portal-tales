baccaa9f5309fe13500e446fc1691c7abff19877
lebed <lebedevegor.ekb@gmail.com>
2026-05-15 16:51:21 +0500

    feat: round test mode for admin

---
+++ fix_edit2.py
@@ -0,0 +1,8 @@
+﻿f = open('src/pages/admin/ScenarioEdit.tsx', encoding='utf-8')
+c = f.read()
+f.close()
+old = 'import { Loader2, Save, ChevronDown, ChevronUp } from "lucide-react";'
+new = 'import { Loader2, Save, ChevronDown, ChevronUp, FlaskConical } from "lucide-react";\nimport { Link } from "react-router-dom";'
+c = c.replace(old, new)
+open('src/pages/admin/ScenarioEdit.tsx', 'w', encoding='utf-8', newline='\n').write(c)
+print('step1 done')
