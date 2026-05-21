287dd65c588177f0167075966b9cce0b020dfce8
lebed <lebedevegor.ekb@gmail.com>
2026-05-21 17:42:34 +0500

    debug: log role assignment

---
+++ plog2.py
@@ -0,0 +1,8 @@
+﻿p='src/pages/Character.tsx'
+c=open(p,encoding='utf-8').read()
+c=c.replace(
+'          setIsSaboteur(pg?.player_roles?.[rp.id] === "saboteur");',
+'          console.log("DEBUG role", {rpId: rp.id, roles: pg?.player_roles, isSab: pg?.player_roles?.[rp.id]});\n          setIsSaboteur(pg?.player_roles?.[rp.id] === "saboteur");'
+)
+open(p,'w',encoding='utf-8',newline='\n').write(c)
+print('ok')
