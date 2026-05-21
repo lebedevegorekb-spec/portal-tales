5762bac60632ea27ea28bc29c9dcbc971dd152fa
lebed <lebedevegor.ekb@gmail.com>
2026-05-22 01:08:01 +0500

    feat: joke variant result + fix export default Scene

---
+++ pfix2.py
@@ -0,0 +1,8 @@
+﻿p='src/pages/Scene.tsx'
+c=open(p,encoding='utf-8').read()
+if 'export default Scene' not in c:
+    c=c+'\nexport default Scene;\n'
+    open(p,'w',encoding='utf-8',newline='\n').write(c)
+    print('added export default')
+else:
+    print('already exists')
