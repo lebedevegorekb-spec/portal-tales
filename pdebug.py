17ec9a552f24eda9a8f5657e8812c433e2a98da6
lebed <lebedevegor.ekb@gmail.com>
2026-05-21 16:13:39 +0500

    debug: log player role

---
+++ pdebug.py
@@ -0,0 +1,8 @@
+﻿p='src/pages/Character.tsx'
+c=open(p,encoding='utf-8').read()
+c=c.replace(
+'          setIsSaboteur(pg?.player_roles?.[rp.id] === "saboteur");',
+'          console.log("rp.id:", rp.id, "player_roles:", JSON.stringify(pg?.player_roles), "match:", pg?.player_roles?.[rp.id]);\n          setIsSaboteur(pg?.player_roles?.[rp.id] === "saboteur");'
+)
+open(p,'w',encoding='utf-8',newline='\n').write(c)
+print('ok')
