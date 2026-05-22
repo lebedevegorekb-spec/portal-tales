80dcc79510eff2491f3a2da1370974d406d9793f
lebed <lebedevegor.ekb@gmail.com>
2026-05-22 21:19:25 +0500

    fix: remove broken replica code from Character.tsx

---
+++ pchar2.py
@@ -0,0 +1,8 @@
+﻿p='src/pages/Character.tsx'
+c=open(p,encoding='utf-8').read()
+c=c.replace(
+'          const intro=(run as any).scenarios?.scenario_json?.party_game?.intro;\n          if(intro?.character_reveal_host_line){setReplicaText(intro.character_reveal_host_line);setReplicaAudio(intro.character_reveal_host_line_audio||undefined);}else{setReplicaDone(true);}\n          ',
+'          '
+)
+open(p,'w',encoding='utf-8',newline='\n').write(c)
+print('ok')
